47a77f28426debbcd1eeac49861371f829dd785b
Atik Hasan <atikhasan801@gmail.com>
2021-11-01 20:05:23 +0600

    last

--- src/firebase/firebaseInit.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-import firebaseConfig from "./firbase.config";
-
-const initializeAuthentication = () => {
-  initializeApp(firebaseConfig);
-};
-
-export default initializeAuthentication;
--- src/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useContext } from "react";
-import { AuthProvider } from "../Contexts/AuthContext";
-
-const useAuth = () => {
-  return useContext(AuthProvider);
-};
-
-export default useAuth;
